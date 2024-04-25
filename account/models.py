dbcd3209115fc08096177dc7c5e0bb434ae30028
Dev-segun2023 <segundavidjohn@gmail.com>
2024-04-25 19:04:13 +0100

    normalized the project

--- account/models.py
+++ account/models.py
@@ -1,26 +1,15 @@
 from django.db import models
-<<<<<<< HEAD
+ 
+from base.models import User
 from base.models import User
 
 
 
 class profile(models.Model):
-=======
-# from django.contrib.auth.models import User
-from base.models import User
-
-
-class Profile(models.Model):
->>>>>>> parent of 4bbdbee (deleted html not in template)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pic')
 
     def __str__(self):
         return f'{self.user.email} profile'
-
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 4bbdbee (deleted html not in template)
 
 # Create your models here.
--- account/signals.py
+++ account/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save
 from base.models import User
 from django.dispatch import receiver
-<<<<<<< HEAD
 from .models import profile
 
 
@@ -9,25 +8,20 @@ from .models import profile
 def create_Profile(sender,instance,created,**kwargs):
     if created:
         profile.objects.create(user=instance)
-=======
-from .models import Profile
-
-
-@receiver(post_save, sender = User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
->>>>>>> parent of 4bbdbee (deleted html not in template)
 
 
 
 
-<<<<<<< HEAD
+
+
+
+
+
+
+
 @receiver(post_save, sender=User)
 def save_Profile(sender,instance,**kwargs):
     instance.profile.save()
-=======
-@receiver(post_save, sender = User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
->>>>>>> parent of 4bbdbee (deleted html not in template)
+
+
+
