151edef765d01c9efdcd6aa1d8ec6244c255ffeb
Dev-segun2023 <segundavidjohn@gmail.com>
2024-05-12 18:38:46 +0100

    added delete functionality and little touches

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import postListView,postDetailView,postCreateView,postUpdateView
+from .views import postListView,postDetailView,postCreateView,postUpdateView,postDeleteView
 from .views import home , display
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
    path('post/<int:pk>/', postDetailView.as_view(), name = 'post-detail'),
    path('post/new/', postCreateView.as_view(), name = 'post-create'),
    path('post/<int:pk>/update/', postUpdateView.as_view(), name = 'post-update'),
+   path('post/<int:pk>/delete/', postDeleteView.as_view(), name = 'post-delete'),
 
    path('base/common.html/',display , name = 'display')
 
--- base/views.py
+++ base/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from account.models import post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import(
     ListView,
     DetailView, 
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
     )
 # Create your views here.
 
@@ -50,6 +51,18 @@ class postUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class postDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = post
+    success_url = '/'
+    template_name = 'post_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def display(request):
     return render(request)
